319ca219579fc11a3d6b309b9ff45dcb0f3ae947
Timothy Lee <tim@codepath.com>
2015-04-15 01:30:36 -0700

    Table view with posters.

--- Rotten Tomatoes/MovieCell.swift
+++ Rotten Tomatoes/MovieCell.swift
@@ -12,6 +12,7 @@ class MovieCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- Rotten Tomatoes/MoviesViewController.swift
+++ Rotten Tomatoes/MoviesViewController.swift
@@ -50,6 +50,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = movie["title"] as? String
         cell.synopsisLabel.text = movie["synopsis"] as? String
         
+        let url = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+        cell.posterView.setImageWithURL(url)
+        
         return cell
     }
 
