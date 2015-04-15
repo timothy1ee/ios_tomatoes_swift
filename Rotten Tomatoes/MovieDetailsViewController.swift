a09a2dbde292ef499c42e411103905d4abf91395
Timothy Lee <tim@codepath.com>
2015-04-15 02:29:47 -0700

    Unscrollable detail page

---
+++ Rotten Tomatoes/MovieDetailsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MovieDetailsViewController.swift
+//  Rotten Tomatoes
+//
+//  Created by Timothy Lee on 4/15/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["synopsis"] as? String
+        
+        let url = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+        imageView.setImageWithURL(url)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
